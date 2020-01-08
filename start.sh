76695a68a741779d32e02d5704a509cc997ea819
Cristiano Coelho Souza <ccoelho.me@gmail.com>
2020-01-08 00:28:36 -0300

    anotações de testes

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 conda env create -f environment.yml
 conda activate vera
 gunicorn --timeout 300 --workers=4 --bind 0.0.0.0:5000 wsgi:app
+
+
+# TESTES:
+# TF-CPU: Detecção 5 imagens: 29s
+# TF-GPU: Detecção 5 imagens: 16s
